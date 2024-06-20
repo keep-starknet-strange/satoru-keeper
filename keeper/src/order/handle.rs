e9ff6100dce6685b8256da7a7774528cadc73de8
zarboq <boramindas@gmail.com>
2024-06-20 17:41:11 +0200

    feat: test price_setup

--- keeper/src/order/handle.rs
+++ keeper/src/order/handle.rs
@@ -5,7 +5,7 @@ use cainome::{
     rs::abigen,
 };
 use starknet::{
-    accounts::{Account, SingleOwnerAccount},
+    accounts::{Account, Call, SingleOwnerAccount},
     core::types::FieldElement,
     providers::jsonrpc::{HttpTransport, JsonRpcClient},
     signers::LocalWallet,
@@ -46,14 +46,22 @@ pub async fn handle_order(
     account: Arc<SingleOwnerAccount<JsonRpcClient<HttpTransport>, LocalWallet>>,
     order: SatoruAction,
 ) {
-    let order_handler_address =
-        env::var("ORDER_HANDLER").expect("ORDER_HANDLER env variable not set");
-    let order_handler = OrderHandler::new(
-        FieldElement::from_hex_be(&order_handler_address)
-            .expect("Conversion error: order_handler_address"),
-        account.clone(),
-    );
+    let set_price_call = get_set_primary_price_call(order.clone(), account.clone()).await;
 
+    let execute_order_call = get_execute_order_call(order, account.clone());
+
+    let order_execution_multicall = account
+        .execute(vec![set_price_call, execute_order_call])
+        .send()
+        .await
+        .expect("Order execution multicall failed");
+    // TODO: poll transaction status
+}
+
+async fn get_set_primary_price_call(
+    order: SatoruAction,
+    account: Arc<SingleOwnerAccount<JsonRpcClient<HttpTransport>, LocalWallet>>,
+) -> Call {
     let data_store_address = env::var("DATA_STORE").expect("DATA_STORE env variable not set");
     let data_store = DataStore::new(
         FieldElement::from_hex_be(&data_store_address)
@@ -75,10 +83,16 @@ pub async fn handle_order(
         .await
         .expect("Could not get market");
 
+    let price = price_setup(order.block_number, market.clone()).await; // TODO use timestamp instead of block number
+
+    oracle.set_primary_price_getcall(&market.long_token, &price)
+}
+
+async fn price_setup(timestamp: u64, market: Market) -> U256 {
     let path = PathParams {
         base: get_token_name_from_address(market.long_token).to_owned(),
         quote: "usd".to_owned(),
-        timestamp: order.block_number, // TODO replace by timestamp
+        timestamp: timestamp,
         interval: "1min".to_owned(),
     };
 
@@ -91,16 +105,25 @@ pub async fn handle_order(
         .await
         .expect("Price did not get returned");
 
-    // Set token price before tx.
-    let price_uint = u128::from_str_radix(price_info.price.as_str(), 16)
+    let price_uint = u128::from_str_radix(price_info.price.as_str().trim_start_matches("0x"), 16)
         .expect("Could not convert hex price to uint");
 
-    let set_price_call = oracle.set_primary_price_getcall(
-        &market.long_token,
-        &U256 {
-            low: price_uint,
-            high: 0,
-        },
+    U256 {
+        low: price_uint,
+        high: 0,
+    }
+}
+
+fn get_execute_order_call(
+    order: SatoruAction,
+    account: Arc<SingleOwnerAccount<JsonRpcClient<HttpTransport>, LocalWallet>>,
+) -> Call {
+    let order_handler_address =
+        env::var("ORDER_HANDLER").expect("ORDER_HANDLER env variable not set");
+    let order_handler = OrderHandler::new(
+        FieldElement::from_hex_be(&order_handler_address)
+            .expect("Conversion error: order_handler_address"),
+        account.clone(),
     );
 
     let set_prices_params: SetPricesParams = SetPricesParams {
@@ -140,15 +163,49 @@ pub async fn handle_order(
         price_feed_tokens: vec![],
     };
 
-    let execute_order_call = order_handler.execute_order_getcall(
+    order_handler.execute_order_getcall(
         &FieldElement::from_hex_be(&order.key).expect("Cannot convert string to felt"),
         &set_prices_params,
-    );
+    )
+}
 
-    let order_execution_multicall = account
-        .execute(vec![set_price_call, execute_order_call])
-        .send()
-        .await
-        .expect("Order execution multicall failed");
-    // TODO: poll transaction status
+#[cfg(test)]
+mod tests {
+    use crate::price::error::PragmaAPIError;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_price_setup() {
+        let api_key = env::var("PRAGMA_API_KEY").or_else(|e| Err(PragmaAPIError::APIKeyNotSet()));
+        match api_key {
+            Ok(_) => {
+                let market = Market {
+                    market_token: ContractAddress::from(
+                        FieldElement::from_hex_be("0x").expect("Cannot convert string to felt"),
+                    ),
+                    index_token: ContractAddress::from(
+                        FieldElement::from_hex_be("0x").expect("Cannot convert string to felt"),
+                    ),
+                    long_token: ContractAddress::from(
+                        FieldElement::from_hex_be(
+                            "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+                        )
+                        .expect("Cannot convert string to felt"),
+                    ),
+                    short_token: ContractAddress::from(
+                        FieldElement::from_hex_be(
+                            "0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8",
+                        )
+                        .expect("Cannot convert string to felt"),
+                    ),
+                };
+
+                let price = price_setup(1711110660, market).await;
+
+                assert!(price > U256 { low: 3000, high: 0 })
+            }
+            Err(_) => {}
+        }
+    }
 }
