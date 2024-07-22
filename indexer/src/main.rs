277c5d98f59fb8c33bf304cd31b54c5d72b81b5a
MrChainBlock <arif.bachir@hotmail.com>
2024-07-22 16:22:34 +0200

    Fix (#64)
    * Fix db error for too bog number
    
    * Externalize from_block configuration to config.rs
    
    * Refactor to encapsulate last block tracking in  struct
    
    * Add support for OrderExecuted event and enhance indexing mechanism
    
    * Externalize contract address to environment variable
    
    * Refactor Order Event Handling
    
    * Add timestamp handling and block variant support for event processing
    
    * fix: database
    
    * fix(indexer): indexing

--- indexer/src/main.rs
+++ indexer/src/main.rs
@@ -15,6 +15,7 @@ use crate::events::{
     deposit::Deposit, market_created::MarketCreated, order::Order, order_executed::OrderExecuted,
     pool_amount_updated::PoolAmountUpdated, swap_fees_collected::SwapFeesCollected,
     swap_info::SwapInfo, withdrawal::Withdrawal, position::Position,
+    swap_info::SwapInfo, withdrawal::Withdrawal, position::Position,
 };
 
 #[tokio::main]
@@ -104,6 +105,7 @@ async fn main() -> Result<(), Error> {
         events::handler::EventIndexer::new(&provider, &pool, event_processors, head_chain);
 
     let mut current_block = start_block;
+    let mut current_block = start_block;
 
     loop {
         let latest_block_on_chain = match provider.block_number().await {
@@ -121,7 +123,23 @@ async fn main() -> Result<(), Error> {
             } else {
                 current_block += 1;
             }
+        let latest_block_on_chain = match provider.block_number().await {
+            Ok(block) => block as i64,
+            Err(e) => {
+                eprintln!("Error fetching latest block number: {:?}", e);
+                sleep(Duration::from_secs(10)).await;
+                continue;
+            }
+        };
+
+        if current_block < latest_block_on_chain {
+            if let Err(e) = indexer.fetch_and_process_events(current_block as u64).await {
+                eprintln!("Error processing pending events: {:?}", e);
+            } else {
+                current_block += 1;
+            }
         }
+
 
         sleep(Duration::from_secs(10)).await;
     }
